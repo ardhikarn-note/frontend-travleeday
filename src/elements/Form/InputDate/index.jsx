f6dba5cb687b8868cfd0e24ad527a504b5614ece
ardhikarn <ardhikarn@gmail.com>
2021-01-02 09:59:17 +0700

    edit extention

--- src/elements/Form/InputDate/index.jsx
+++ src/elements/Form/InputDate/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import propTypes from "prop-types";
 
 import { DateRange } from "react-date-range";
-
+// import css/scss tidak perlu from
 import "./index.scss";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
